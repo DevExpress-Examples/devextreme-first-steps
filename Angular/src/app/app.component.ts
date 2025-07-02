e807788e6d8d4d73a8326fbac3d2d5d914e5e87a
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-07-02 11:32:33 +0400

    Add Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { DxDataGridComponent, DxiDataGridColumnComponent, DxoDataGridEditingComponent } from 'devextreme-angular/ui/data-grid';
+import { DxProgressBarComponent } from 'devextreme-angular/ui/progress-bar';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [DxDataGridComponent, DxProgressBarComponent, DxiDataGridColumnComponent, DxoDataGridEditingComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular';
-
-  counter = 0;
-
-  buttonText = 'Click count: 0';
-
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
-  }
+  tasks = [
+    { id: 1, task: "Buy groceries", dueDate: new Date(), done: false },
+    { id: 2, task: "Write a blog post", dueDate: new Date(), done: true }
+  ];
+  progressValue = 50;
+  updateProgress() {
+    const all = this.tasks.length;
+    const completed = this.tasks.filter((t) => t.done).length;
+    this.progressValue = Math.round((completed / all) * 100);
+  };
 }
+
