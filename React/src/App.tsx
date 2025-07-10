9a972aee34fa80c19643bb67afdbd2f7ff32e5bf
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-07-10 13:09:29 +0400

    Try to fix lint

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,40 +1,50 @@
-import { useState } from "react";
-import DataGrid, { Column, Editing } from "devextreme-react/data-grid";
-import ProgressBar from "devextreme-react/progress-bar";
-import "./App.css";
-import "devextreme/dist/css/dx.light.css";
+import { useState } from 'react';
+import DataGrid, { Column, Editing } from 'devextreme-react/data-grid';
+import ProgressBar from 'devextreme-react/progress-bar';
+import './App.css';
+import 'devextreme/dist/css/dx.light.css';
 
 const tasks = [
-  { id: 1, task: "Buy groceries", dueDate: new Date(), done: false },
-  { id: 2, task: "Write a blog post", dueDate: new Date(), done: true },
+  {
+    id: 1,
+    task: 'Buy groceries',
+    dueDate: new Date(),
+    done: false,
+  },
+  {
+    id: 2,
+    task: 'Write a blog post',
+    dueDate: new Date(),
+    done: true,
+  },
 ];
 
 function App(): JSX.Element {
   const [progressValue, setProgressValue] = useState(50);
-  function updateProgress() {
+  function updateProgress(): void {
     const all = tasks.length;
     const completed = tasks.filter((t) => t.done).length;
     setProgressValue(Math.round((completed / all) * 100));
   }
   return (
-    <div id="dashboard">
-      <ProgressBar id="progress" value={progressValue} />
+    <div id='dashboard'>
+      <ProgressBar id='progress' value={progressValue} />
       <DataGrid
-        id="task-grid"
+        id='task-grid'
         dataSource={tasks}
         onRowUpdated={updateProgress}
         onRowInserted={updateProgress}
         onRowRemoved={updateProgress}
       >
-        <Column dataField="task" />
-        <Column dataField="dueDate" />
-        <Column dataField="done" />{" "}
+        <Column dataField='task' />
+        <Column dataField='dueDate' />
+        <Column dataField='done' />{' '}
         <Editing
-          mode="row"
+          mode='row'
           allowUpdating={true}
           allowAdding={true}
           allowDeleting={true}
-          newRowPosition="last"
+          newRowPosition='last'
         />
       </DataGrid>
     </div>
