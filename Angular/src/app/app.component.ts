4281f3c9cf22ee4fa775581ce264fe7022b85a85
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-07-10 12:13:48 +0400

    try to fix lint

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,18 +7,29 @@ import { DxProgressBarComponent } from 'devextreme-angular/ui/progress-bar';
   standalone: true,
   imports: [DxDataGridComponent, DxProgressBarComponent, DxiDataGridColumnComponent, DxoDataGridEditingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   tasks = [
-    { id: 1, task: "Buy groceries", dueDate: new Date(), done: false },
-    { id: 2, task: "Write a blog post", dueDate: new Date(), done: true }
+    { 
+      id: 1, 
+      task: 'Buy groceries', 
+      dueDate: new Date(), 
+      done: false,
+    },
+    { 
+      id: 2, 
+      task: 'Write a blog post', 
+      dueDate: new Date(), 
+      done: true,
+    }
   ];
+
   progressValue = 50;
+
   updateProgress() {
     const all = this.tasks.length;
     const completed = this.tasks.filter((t) => t.done).length;
     this.progressValue = Math.round((completed / all) * 100);
-  };
+  }
 }
-
