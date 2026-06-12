e055ffe157795ad8add4c78f423c1674018dda31
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:21:20 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 import { DxProgressBarModule } from 'devextreme-angular/ui/progress-bar';
 import { DxButtonModule, type DxButtonTypes } from 'devextreme-angular/ui/button';
@@ -18,6 +18,7 @@ interface TaskData {
   standalone: true,
   imports: [DxDataGridModule, DxProgressBarModule, DxButtonModule],
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
